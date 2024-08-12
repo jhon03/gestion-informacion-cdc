b7b87043d247ac71bcf939c77c9a228e7d23c007
jhon03 <hoyosb90@gmail.com>
2024-08-11 20:32:11 -0500

    rutas corregido y arreglado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,47 @@
 
 
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { from } from 'rxjs';
 import { ProgramaListComponent } from './presentation/components/programa/programa-list/programa-list.component';
 import { DetalleProgramaComponent } from './presentation/detalles-programa/componentes/detalle-programa/detalle-programa.component';
+import { PaginaInicioComponent } from './presentation/components/pagina-inicio/pagina-inicio.component';
+import { InfoCdcComponent } from './presentation/components/info-cdc/info-cdc.component';
+import { EscenarioProblemaComponent } from './presentation/components/escenario-problema/escenario-problema.component';
 
 
 
 export const routes: Routes = [
+   
+    {
+       path: '',
+      component: PaginaInicioComponent
+    },
+ 
+    {
+        path: 'home',
+        component: PaginaInicioComponent
+    },
 
+    {
+        path: 'info-cdc',
+        component: InfoCdcComponent
+    },
+    
+    {
+        path: 'escenario', 
+        component: EscenarioProblemaComponent
+    },
 
+    {
+        path: 'usuarios',
+        loadChildren: () => import('./presentation/modules/usuarios/usuarios.module').then(m => m.UsuariosModule)
+    },
+    
     {
         path: 'formato-registro',
         loadChildren: ()=> import('./presentation/formatos-de-registros/formatos-de-registros.module').then(m => m.FormatosDeRegistrosModule)
     },
-
-   
 
     {
         path: 'evaluacion-activacion-programa',
@@ -24,26 +49,20 @@ export const routes: Routes = [
 
     },
 
-    {
-        path: 'usuarios',
-        loadChildren: () => import('./presentation/modules/usuarios/usuarios.module').then(m => m.UsuariosModule)
+
+    { path: 'detalles-programa', 
+    component: DetalleProgramaComponent
+    },
+    
+    { path: 'programas',
+        
+     component: ProgramaListComponent
     },
 
-    { path: 'detalles-programa', component: DetalleProgramaComponent},
-
-    { path: '', redirectTo: '/formato-registro', pathMatch: 'full'},
-    { path: '**', redirectTo: '/formato-registro'},
-
-    
-    {path: '', redirectTo: '/evaluacion-activacion-programa', pathMatch: 'full'},
-    {path: '**', redirectTo: '/evaluacion-activacion-programa'},
-
-    { path: 'detalles-programa', component: DetalleProgramaComponent},
-    {path: '', redirectTo: '/detalles-programa', pathMatch: 'full'},
-
-
-    { path: 'programas', component: ProgramaListComponent},
-    {path: '', redirectTo: '/progrmas', pathMatch: 'full'},
+    {
+        path: '**',
+        redirectTo: ''
+    },
 
 ];
 
