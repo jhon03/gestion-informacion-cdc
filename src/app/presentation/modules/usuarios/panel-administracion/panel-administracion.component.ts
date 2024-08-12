0ea6815fb740eb4455ee2dd7dbfac14e186b6b46
jhon03 <hoyosb90@gmail.com>
2024-08-11 20:34:26 -0500

    configuración de componenente para administracion de usuarios

--- src/app/presentation/modules/usuarios/panel-administracion/panel-administracion.component.ts
+++ src/app/presentation/modules/usuarios/panel-administracion/panel-administracion.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MaterialModule } from '../../material/material/material.module';
+import { MatSidenav } from '@angular/material/sidenav';
+import { PaginaInicioComponent } from '../../../components/pagina-inicio/pagina-inicio.component';
+import { InfoCdcComponent } from '../../../components/info-cdc/info-cdc.component';
+import { EscenarioProblemaComponent } from '../../../components/escenario-problema/escenario-problema.component';
+import { CrearColaboradorComponent } from '../colaborador/crear-colaborador/crear-colaborador.component';
+import { RolesComponent } from '../roles/roles.component';
+
 @Component({
   selector: 'app-panel-administracion',
   standalone: true,
   imports: [
- MaterialModule
+ MaterialModule,
+ CrearColaboradorComponent,
+ RolesComponent
   ],
   templateUrl: './panel-administracion.component.html',
   styleUrl: './panel-administracion.component.css'
@@ -12,5 +21,6 @@ import { MaterialModule } from '../../material/material/material.module';
 export class PanelAdministracionComponent {
 
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
   
   }
