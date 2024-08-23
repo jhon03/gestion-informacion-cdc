84d1f1b0d3188bf081acdb685c05ed6bf1b789b9
jhon03 <hoyosb90@gmail.com>
2024-08-23 01:12:26 -0500

    impl. botón login en la página de inicio

--- src/app/presentation/components/pagina-inicio/pagina-inicio.component.ts
+++ src/app/presentation/components/pagina-inicio/pagina-inicio.component.ts
@@ -3,14 +3,15 @@ import { MaterialModule } from '../../modules/material/material/material.module'
 import { InfoCdcComponent } from '../info-cdc/info-cdc.component';
 import { MatSidenav } from '@angular/material/sidenav';
 import { EscenarioProblemaComponent } from '../escenario-problema/escenario-problema.component';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-pagina-inicio',
   standalone: true,
   imports: [ 
     MaterialModule,
     InfoCdcComponent, 
-    EscenarioProblemaComponent
+    EscenarioProblemaComponent,
+    RouterModule
   ],
   templateUrl: './pagina-inicio.component.html',
   styleUrl: './pagina-inicio.component.css'
@@ -22,13 +23,5 @@ export class PaginaInicioComponent {
 
   constructor(private router: Router){}
 
-  navigateToLogin(): void{
-    this.router.navigate(['/login']).then(success => {
-      if (success){
-        console.log('navegación sactisfatoria');
-      } else {
-        console.error('navegación ha fallado');
-      }
-    });
-  }
 }
+
